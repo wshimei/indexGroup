b4efaa7a82d960a1d7ccf5b0ac2e02d861dab67a
Shimei Wong <wshimei@gmail.com>
2017-08-08 13:25:47 +0800

    carousel height set to 2000px

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@
 $(document).ready(function () {
   $('.dropdown-button').dropdown()
   $('.parallax').parallax()
-  $('#tabs-swipe-demo').tabs({ 'swipeable': true })
+  $('#tabs-swipe').tabs({ 'swipeable': true })
+
+  $('.carousel').height('2200px')
 })
